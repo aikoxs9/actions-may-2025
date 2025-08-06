753a44262617d11e4cbcbd61a491805818febe3c
aikoxs9 <ainuskamursabek@gmaill.com>
2025-08-06 23:29:16 +0000

    add actions

---
+++ main.tf
@@ -0,0 +1,17 @@
+provider aws {
+    region = "us-east-2"
+}
+
+resource aws_instance web {
+    ami           = "ami-08ca1d1e465fbfe0c"
+    instance_type = "t3.micro"
+}
+
+
+terraform {
+    backend "s3" {
+        bucket = "kaizen-ainuskam"
+        key = "terraform.tfstate"
+        region = "us-east-1"
+}
+}
